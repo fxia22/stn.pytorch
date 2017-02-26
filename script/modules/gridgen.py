0bf20442b03b743818130ee3e6901f774613f15b
fxia22 <xf1280@gmail.com>
2017-02-26 00:51:32 -0800

    add aux loss

--- script/modules/gridgen.py
+++ script/modules/gridgen.py
@@ -6,19 +6,35 @@ from functions.gridgen import AffineGridGenFunction, CylinderGridGenFunction
 
 
 class AffineGridGen(Module):
-    def __init__(self, height, width, lr = 1):
+    def __init__(self, height, width, lr = 1, aux_loss = False):
         super(AffineGridGen, self).__init__()
         self.height, self.width = height, width
+        self.aux_loss = aux_loss
         self.f = AffineGridGenFunction(self.height, self.width, lr=lr)
     def forward(self, input):
-        return self.f(input)
+        if not self.aux_loss:
+            return self.f(input)
+        else:
+            identity = torch.from_numpy(np.array([[1,0,0], [0,1,0]], dtype=np.float32))
+            batch_identity = torch.zeros([input.size(0), 2,3])
+            for i in range(input.size(0)):
+                batch_identity[i] = identity
+            batch_identity = Variable(batch_identity)
+            loss = torch.mul(input - batch_identity, input - batch_identity)
+            loss = torch.sum(loss,1)
+            loss = torch.sum(loss,2)
 
-
+            return self.f(input), loss.view(-1,1)
 
 class CylinderGridGen(Module):
-    def __init__(self, height, width, lr = 1):
+    def __init__(self, height, width, lr = 1, aux_loss = False):
         super(CylinderGridGen, self).__init__()
         self.height, self.width = height, width
+        self.aux_loss = aux_loss
         self.f = CylinderGridGenFunction(self.height, self.width, lr=lr)
     def forward(self, input):
-        return self.f(input)
+        
+        if not self.aux_loss:
+            return self.f(input)
+        else:
+            return self.f(input), torch.mul(input, input).view(-1,1)
--- script/test.py
+++ script/test.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 from torch.autograd import Variable
 
 from modules.stn import STN
-from modules.gridgen import AffineGridGen
+from modules.gridgen import AffineGridGen, CylinderGridGen
 
 nframes = 64
 height = 64
@@ -22,12 +22,13 @@ input2.data.uniform_(-1,1)
 input = Variable(torch.from_numpy(np.array([[[0.8, 0.3, 1], [0.5, 0, 0]]], dtype=np.float32)), requires_grad = True)
 print input
 
-g = AffineGridGen(64, 128)
-out = g(input)
+g = AffineGridGen(64, 128, aux_loss = True)
+out, aux = g(input)
 print out.size()
 print out
 out.backward(out.data)
 print input.grad
+
 
 
 #print input2.data
@@ -36,3 +37,12 @@ out = s(input1, input2)
 #print out
 out.backward(input1.data)
 print input1.grad.size()
+
+
+input = Variable(torch.from_numpy(np.array([[3.6]], dtype=np.float32)), requires_grad = True)
+print input
+
+g = CylinderGridGen(64, 128, aux_loss = True)
+out, aux = g(input)
+print out.size()
+print out
