89d6a997c7b49f66b30fa1b6732166ecbd61dcf0
fxia22 <xf1280@gmail.com>
2017-03-02 13:19:29 -0800

    update cylinder grid gen

--- script/modules/gridgen.py
+++ script/modules/gridgen.py
@@ -66,6 +66,35 @@ class AffineGridGenV2(Module):
         
         return output
     
+    
+class CylinderGridGenV2(Module):
+    def __init__(self, height, width, lr = 1):
+        super(CylinderGridGenV2, self).__init__()
+        self.height, self.width = height, width
+        self.lr = lr
+        self.grid = np.zeros( [self.height, self.width, 3], dtype=np.float32)
+        self.grid[:,:,0] = np.expand_dims(np.repeat(np.expand_dims(np.arange(-1, 1, 2.0/self.height), 0), repeats = self.width, axis = 0).T, 0)
+        self.grid[:,:,1] = np.expand_dims(np.repeat(np.expand_dims(np.arange(-1, 1, 2.0/self.width), 0), repeats = self.height, axis = 0), 0)
+        self.grid[:,:,2] = np.ones([self.height, width])
+        self.grid = torch.from_numpy(self.grid.astype(np.float32))
+    def forward(self, input):
+        self.batchgrid = torch.zeros(torch.Size([input.size(0)]) + self.grid.size() )
+        #print(self.batchgrid.size())
+        for i in range(input.size(0)):
+            self.batchgrid[i,:,:,:] = self.grid
+        self.batchgrid = Variable(self.batchgrid)
+        
+        #print(self.batchgrid.size())
+        
+        input_u = input.view(-1,1,1,1).repeat(1,self.height, self.width,1)
+        #print(input_u.requires_grad, self.batchgrid)
+        output = Variable(torch.zeros(torch.Size([input.size(0)]) + self.grid.size()[:2] + torch.Size([2])), requires_grad = True)
+        for i in range(input.size(0)):
+            output[i,:,:,0] = self.batchgrid[i,:,:,0]
+            output[i,:,:,1] = torch.atan(torch.tan(np.pi/2.0*(self.batchgrid[i,:,:,1] + self.batchgrid[i,:,:,2] * input_u[i,:,:,:])))  /(np.pi/2)
+        
+        return output
+
 
 class DenseAffineGridGen(Module):
     def __init__(self, height, width, lr = 1, aux_loss = False):
